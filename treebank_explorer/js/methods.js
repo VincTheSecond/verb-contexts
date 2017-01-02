d8671bc2c118647816b97c5c48ebdcf29a989a9e
Vincent Kriz <vincent.kriz@kamadu.eu>
2017-01-02 12:50:14 +0100

    Removed debug alert.

--- treebank_explorer/js/methods.js
+++ treebank_explorer/js/methods.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function() {
     jQuery('#search').click(function() {
         sent_id = jQuery('#sent_id').val();
-        alert(sent_id);
         jQuery('#tree').attr('src', './html/' + sent_id + '.conllu.html')
     });
 });
